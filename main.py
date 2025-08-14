09006d5530a316a5ee806c94aec8596a6bcd6bcf
bobopeut <72267557+bobopeut@users.noreply.github.com>
2025-08-14 15:16:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ EMAILS = [
     "contact@peutevynck-digitalize.fr", "contact@bpc-analytics.fr", "contact@boris-consult.fr",
     "contact@proxyclean-services.com", "contact@emb-informatique.fr", "contact@rhdepochepro.fr",
     "contact@balioavantages.fr", "contact@univers-viager-consult.fr","contact@airzen-radio.fr",
-    "contact@webecode.fr", "contact@lmz-prod.fr","contact@technovr-leonard.fr", "contact@lmz-prod.fr","contact@domainedaugeron.fr","contact@champagne-yves-loison.fr","contact@nextbroadcastmedia-audio.be","contact@chateaulagorce.fr","contact@tamsprod.fr","contact@design-leo.fr","contact@bergerac-us-tennis.fr","contact@chateau-de-peyrel.fr",
+    "contact@webecode.fr", "contact@lmz-prod.fr","contact@technovr-leonard.fr", "contact@lmz-prod.fr","contact@domainedaugeron.fr","contact@champagne-yves-loison.fr","contact@nextbroadcastmedia-audio.be","contact@chateaulagorce.fr","contact@tamsprod.fr","contact@design-leo.fr","contact@bergerac-us-tennis.fr","contact@chateau-de-peyrel.fr","contact@biosalines-cosmetique.fr"
 ]
 
 # Mot de passe général
@@ -63,6 +63,7 @@ SPECIFIC_PASSWORDS = {
     "contact@design-leo.fr": "LEODESIGN2025.",
     "contact@bergerac-us-tennis.fr": "USBTENNIS2025.",
     "contact@chateau-de-peyrel.fr": "Peyrel24130",
+    "contact@biosalines-cosmetique.fr": "BIOSALINES2025.",
 }
 
 IMAP_SERVER = "ssl0.ovh.net"
